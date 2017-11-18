05cb55da67fb3faae006f19f9e76ed43bc4fe28e
Allison <allisonverdam@gmail.com>
2017-11-18 20:31:32 -0200

    adicionando mais uns registros no banco para ficar mais fácil testar as funcionalidades

--- src/main/java/com/mensageria/MensageriaApplication.java
+++ src/main/java/com/mensageria/MensageriaApplication.java
@@ -10,10 +10,12 @@ import com.mensageria.model.Conversa;
 import com.mensageria.model.Dispositivo;
 import com.mensageria.model.Instituicao;
 import com.mensageria.model.Mensagem;
+import com.mensageria.model.Participa;
 import com.mensageria.model.Usuario;
 import com.mensageria.services.ConversaService;
 import com.mensageria.services.DispositivoService;
 import com.mensageria.services.MensagemService;
+import com.mensageria.services.ParticipaService;
 import com.mensageria.services.UsuarioService;
 
 @SpringBootApplication
@@ -25,27 +27,40 @@ public class MensageriaApplication {
 
 	@Bean
 	public String popularBanco(MensagemService serviceMensagem, UsuarioService serviceUsuario,
-			ConversaService serviceConversa, DispositivoService serviceDispositivo) {
+			ConversaService serviceConversa, DispositivoService serviceDispositivo, ParticipaService serviceParticipa) {
 
 		if (!serviceUsuario.findAll().isEmpty()) {
 			return "banco ja foi populado";
 		}
 
-		serviceUsuario.save(new Usuario("email", true, "nome", Calendar.getInstance()));
+		serviceUsuario.save(new Usuario("allison@g.com", true, "Allison", Calendar.getInstance()));
+		serviceUsuario.save(new Usuario("renan@g.com", true, "Renan", Calendar.getInstance()));
+		serviceUsuario.save(new Usuario("gabriel@g.com", true, "Gabriel", Calendar.getInstance()));
 		serviceUsuario.save(new Instituicao("email 2", true, "nome 2", Calendar.getInstance(), "cnpj"));
 
-		serviceConversa.save(new Conversa(true, "conversa", Calendar.getInstance()));
-		serviceConversa.save(new Conversa(false, "conversa 2", Calendar.getInstance()));
-
+		serviceConversa.save(new Conversa(true, "conversa 1", Calendar.getInstance()));
+		serviceConversa.save(new Conversa(true, "conversa 2", Calendar.getInstance()));
+		serviceConversa.save(new Conversa(false, "conversa 3", Calendar.getInstance()));
+		
+		serviceParticipa.save(new Participa(serviceUsuario.findAll().get(0), serviceConversa.findAll().get(0)));
+		serviceParticipa.save(new Participa(serviceUsuario.findAll().get(1), serviceConversa.findAll().get(0)));
+		
+		serviceParticipa.save(new Participa(serviceUsuario.findAll().get(0), serviceConversa.findAll().get(1)));
+		serviceParticipa.save(new Participa(serviceUsuario.findAll().get(2), serviceConversa.findAll().get(1)));
+		
 		serviceDispositivo.save(new Dispositivo("mac", "nome", "token", serviceUsuario.findAll().get(0)));
 		serviceDispositivo.save(new Dispositivo("mac2", "nome2", "token", serviceUsuario.findAll().get(0)));
 
 		serviceMensagem.save(new Mensagem("conteudo", Calendar.getInstance(), serviceUsuario.findAll().get(0),
 				serviceConversa.findAll().get(0)));
 		serviceMensagem.save(new Mensagem("conteudo 2", Calendar.getInstance(), serviceUsuario.findAll().get(0),
-				serviceConversa.findAll().get(1)));
+				serviceConversa.findAll().get(2)));
 		serviceMensagem.save(new Mensagem("conteudo 3", Calendar.getInstance(), serviceUsuario.findAll().get(0),
 				serviceConversa.findAll().get(0)));
+		serviceMensagem.save(new Mensagem("Olá, Gabriel!", Calendar.getInstance(), serviceUsuario.findAll().get(0),
+				serviceConversa.findAll().get(1)));
+		serviceMensagem.save(new Mensagem("Oi, Allison!", Calendar.getInstance(), serviceUsuario.findAll().get(2),
+				serviceConversa.findAll().get(1)));
 		
 		return "banco populado";
 	}
