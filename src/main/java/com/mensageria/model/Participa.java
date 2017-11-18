dae1c3a99e25578b70e1714cb03a32006901834d
Allison <allisonverdam@gmail.com>
2017-11-18 20:31:10 -0200

    implementando getters e setters e o construtor

--- src/main/java/com/mensageria/model/Participa.java
+++ src/main/java/com/mensageria/model/Participa.java
@@ -16,4 +16,33 @@ public class Participa {
 	private Usuario usuario;
 	@ManyToOne
 	private Conversa conversa;
+	
+	public Participa(Usuario usuario, Conversa conversa) {
+		super();
+		this.usuario = usuario;
+		this.conversa = conversa;
+	}
+	public Participa() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Conversa getConversa() {
+		return conversa;
+	}
+	public void setConversa(Conversa conversa) {
+		this.conversa = conversa;
+	}
+	
+	
 }
