a52e7daa11432188115140634fd1cdd7a6669f53
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-12-03 06:31:13 -0200

    Fazendo varias coisas relacionadas a conversa

--- src/main/java/com/mensageria/controller/ConversaController.java
+++ src/main/java/com/mensageria/controller/ConversaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,10 @@ public class ConversaController {
 	@RequestMapping(value = "/conversas", method = RequestMethod.GET)
 	public List<Conversa> getAllConversas() {
 		return conversaService.findAll();
+	}
+	
+	@RequestMapping(value = "/conversas", method = RequestMethod.POST)
+	public Conversa cadastrarConversa(@RequestBody Conversa conversa) {
+		return conversaService.save(conversa);
 	}
 }
--- src/main/java/com/mensageria/services/ConversaService.java
+++ src/main/java/com/mensageria/services/ConversaService.java
@@ -1,5 +1,7 @@
 package com.mensageria.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +13,30 @@ public class ConversaService extends GenericService<Conversa> {
 
 	@Autowired
 	ConversaRepository repository;
+	
+	public Conversa findOne(Long id) {
+		Conversa tempConversa = repository.findOne(id);
+		tempConversa.setQtdMensagens(qtdMensagensConversa(tempConversa.getId()));
+		tempConversa.setQtdPessoas(qtdPessoasConversa(tempConversa.getId()));
+		return tempConversa;
+	}
+	
+	public List<Conversa> findAll(){
+		List<Conversa> conversas = repository.findAll();
+		
+		for(Conversa conversa: conversas) {
+			conversa.setQtdMensagens(qtdMensagensConversa(conversa.getId()));
+			conversa.setQtdPessoas(qtdPessoasConversa(conversa.getId()));
+		}
+		
+		return conversas;
+	}
+	
+	int qtdPessoasConversa(Long id) {
+		return repository.qtdPessoasConversa(id);
+	}
+	
+	int qtdMensagensConversa(Long id) {
+		return repository.qtdMensagensConversa(id);
+	}
 }
