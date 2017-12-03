81fd51c1b0aa9dd5b7ac458a9b65e878896f5e87
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-12-02 23:11:47 -0200

    Endpoint para cadastrar usuarios

--- src/main/java/com/mensageria/controller/UsuarioController.java
+++ src/main/java/com/mensageria/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.mensageria.controller;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +22,7 @@ import com.mensageria.services.UsuarioService;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 public class UsuarioController implements MetodosUsuario {
 
 	@Autowired
@@ -45,20 +45,19 @@ public class UsuarioController implements MetodosUsuario {
 		return null;
 	}
 
-	
 	@RequestMapping(value = "/registrar", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	@ResponseBody
 	@Override
 	public Dispositivo registrar(@RequestBody Dispositivo dispositivo) {
 		dispositivo.setPushToken(dispositivoService.gerarToken());
-		
+
 		Calendar dataAtual = Calendar.getInstance();
 		dispositivo.setDataRegistro(dataAtual);
 		dispositivo.getProprietario().setUltimoAcesso(dataAtual);
 		dispositivo.getProprietario().setPrioridade(0);
-	
+
 		dispositivo.setProprietario(this.usuarioService.save(dispositivo.getProprietario()));
-		
+
 		return this.dispositivoService.save(dispositivo);
 	}
 
@@ -67,9 +66,19 @@ public class UsuarioController implements MetodosUsuario {
 	public List<Dispositivo> getAllDispositivos() {
 		return this.dispositivoService.findAll();
 	}
-	
+
+	@RequestMapping(value = "/usuarios", method = RequestMethod.POST)
+	public List<Usuario> cadastrarUsuarios(@RequestBody List<Usuario> usuarios) {
+		List<Usuario> usuarioCadastrados = new ArrayList<Usuario>();
+		for (Usuario usuario : usuarios) {
+			usuarioCadastrados.add(usuarioService.save(usuario));
+		}
+
+		return usuarioCadastrados;
+	}
+
 	@RequestMapping("/usuarios/{usuarioId}")
-	public Usuario getUsuario(@PathVariable Long usuarioId){
+	public Usuario getUsuario(@PathVariable Long usuarioId) {
 		return usuarioService.findById(usuarioId);
 	}
 }
